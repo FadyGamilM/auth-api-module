5ca6ecd096baa46e183cc646e51e261346f16a02
FadyGamilM <gamilfady605@gmail.com>
2023-03-26 15:44:29 +0200

    handle general errors

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response } from "express"
-import { errorHandler } from "./middleware/error"
-import morgan from "morgan"
+import express, { Application, Request, Response } from "express";
+import { errorHandler } from "./middleware/error";
+import morgan from "morgan";
 
 // import rate-limiter middleware
-import RateLimit from "express-rate-limit"
+import RateLimit from "express-rate-limit";
 
-const app: Application = express()
+const app: Application = express();
 
 // parse the incoming requests
-app.use(express.json())
+app.use(express.json());
 
 // use morgan logger middleware
-app.use(morgan("common"))
+app.use(morgan("common"));
 
 // use the rate limit middleware
 app.use(
@@ -20,28 +20,25 @@ app.use(
         max: 200,
         standardHeaders: true,
         legacyHeaders: false,
-        message: "too many requests from th IP address, try again after 15 mins !"
+        message: "too many requests from th IP address, try again after 15 mins !",
     })
-)
+);
 
 app.get("/", (req: Request, res: Response) => {
-    return res.sendStatus(200)
-})
+    return res.sendStatus(200);
+});
 
 //! handle the 404 not found error
 app.use((_req: Request, res: Response) => {
-    return res.status(404).json(
-        {
+    return res.status(404).json({
+        mesage: "Not Found, Make sure you hit the right url",
+    });
+});
 
-            mesage: "Not Found, Make sure you hit the right url"
-        }
-    )
-})
+app.use(errorHandler);
 
-app.use(errorHandler)
-
-const PORT = 5050
+const PORT = 5050;
 
 app.listen(PORT, () => {
-    console.log("app is up and running")
-})
+    console.log("app is up and running");
+});
